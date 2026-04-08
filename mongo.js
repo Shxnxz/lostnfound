b8b2f9fe52a5095d016098d22ed51d2114c8b249
Shxnxz <138223066+Shxnxz@users.noreply.github.com>
2026-04-08 14:38:48 +0700

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dbuser:lostnfound1@lostnfound.vxj4u69.mongodb.net/?appName=lostnfound";
+const uri = "mongodb+srv://dbuser:lostnfound1@lostnfound.3jwt2lm.mongodb.net/?appName=lostnfound";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
